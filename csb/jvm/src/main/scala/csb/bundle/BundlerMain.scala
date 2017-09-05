88d084057fe463b895a6922cabe2bafc44ed3e04
Ludovic Guegan <ludovic.guegan@gmail.com>
2017-09-05 21:53:34 +0200

    generate concatenated target/Player.scala file

---
+++ csb/jvm/src/main/scala/csb/bundle/BundlerMain.scala
@@ -0,0 +1,13 @@
+package csb.bundle
+
+object BundlerMain {
+  def main(args: Array[String]): Unit = {
+    if (args.isEmpty) {
+      println("Input file name must be provided")
+      sys.exit(1)
+    }
+    args.foreach { fileName =>
+      Bundler(fileName, StdBundlerIo()).bundle()
+    }
+  }
+}
